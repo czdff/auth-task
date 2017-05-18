ec07b92ff879c766c394214819c38de5dc4f1b3a
Vasyl <admin@gmail.com>
2017-05-18 19:14:52 +0300

    create task controller

--- app/Task.php
+++ app/Task.php
@@ -13,4 +13,11 @@ class Task extends Model {
      */
     protected $fillable = ['name'];
 
+    /**
+     * Получить пользователя - владельца данной задачи
+     */
+    public function user() {
+	return $this->belongsTo(User::class);
+    }
+
 }
