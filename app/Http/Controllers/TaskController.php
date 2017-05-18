fffb37fefa1a5c60b101bbb201d7211977dc194b
Vasyl <admin@gmail.com>
2017-05-18 19:27:35 +0300

    made something

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,10 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Http\Requests;
 
-class TaskController extends Controller
-{
-    //
+class TaskController extends Controller {
+
+    /**
+     * Создание нового экземпляра контроллера.
+     *
+     * @return void
+     */
+    public function __construct() {
+	$this->middleware('auth');
+    }
+
+    /**
+     * Отображение списка всех задач пользователя.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request) {
+	return view('tasks.index');
+    }
+
 }
